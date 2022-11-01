f9f2c2bc4893f048f67d7d90a8c1ed855ddd4e55
perxpective <ervinlee2801@gmail.com>
2022-11-01 19:15:38 +0800

    fixed font optimisation

--- components/font.js
+++ components/font.js
@@ -1,8 +1,8 @@
-import { Global } from '@emotion/react'
-const Fonts = () => {
-    <Global
-       styles={`@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');`} 
-    />
-}
-
+const Fonts = () => (
+  <style jsx global>{`
+    @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+  `}</style>
+)
 export default Fonts
+
+//styles={`@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');`} 
