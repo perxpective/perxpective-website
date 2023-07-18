7809fa551909baf55779f0629385e6cc4a0dced8
Ervin Lee <ervinlee.21@ichat.sp.edu.sg>
2023-07-18 10:06:57 +0800

    change align to left

--- pages/career.js
+++ pages/career.js
@@ -243,7 +243,7 @@ export default function Career() {
                                 
                                 <Center>
                                     <Flex direction="column" alignItems="center">
-                                        <Flex alignItems="center">
+                                        <Flex alignItems="left">
                                             <Box width="50px" height="50px" marginRight="10px">
                                                 <Img borderRadius={20} src="https://www.mindef.gov.sg/oms/dis/images/identity/logo/logo-dis-crest01.png" w={10} objectFit="cover" />
                                             </Box>
